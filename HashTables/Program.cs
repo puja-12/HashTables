a80781c50b33e1b316c8fb124c9a0692b4eafe87
puja <pujarana1796@gmail.com>
2022-05-14 10:14:50 +0530

    puja added first commit

--- HashTables/Program.cs
+++ HashTables/Program.cs
@@ -10,23 +10,46 @@ namespace HashTables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to hashtable");
-            mymapNode<string, string> hash = new mymapNode<string, string>(5);
-            hash.Add("0", "To");
-            hash.Add("1", "be");
-            hash.Add("2", "or");
-            hash.Add("3", "not");
-            hash.Add("4", "to");
-            hash.Add("5", "be");
-             string hash5 = hash.Get("5");
-             Console.WriteLine("5th index value: " + hash5);
+            while (true)
+            {
+                Console.WriteLine("select no\n1)index value of words\n2)index value of paragraph\n");
 
-             string hash2 = hash.Get("2");
-             Console.WriteLine("2nd  index value: " + hash2);
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("welcome to hashtable");
+                        mymapNode<string, string> hash = new mymapNode<string, string>(5);
+                        hash.Add("0", "To");
+                        hash.Add("1", "be");
+                        hash.Add("2", "or");
+                        hash.Add("3", "not");
+                        hash.Add("4", "to");
+                        hash.Add("5", "be");
+                        string hash5 = hash.Get("5");
+                        Console.WriteLine("5th index value: " + hash5);
 
-        }
+                        string hash2 = hash.Get("2");
+                        Console.WriteLine("2nd  index value: " + hash2);
+                        break;
+                    case 2:
+                        mymapNode<string, string> hash3 = new mymapNode<string, string>(5);
+                        String testing = "Paranoids are not paranoid  because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable  situations";
 
+                        Console.Write(testing.IndexOf("because") + Environment.NewLine);
+
+                        Console.WriteLine(testing.Substring(testing.IndexOf("because")));
+                                          
+                           Console.WriteLine("Occurrence:" + hash3.CheckOccurrences(testing, "because"));
+                        
+
+                        break;
+
+                }
+
+            }
         }
     }
+}
 
        
--- HashTables/mymapNode.cs
+++ HashTables/mymapNode.cs
@@ -6,63 +6,86 @@ using System.Threading.Tasks;
 
 namespace HashTables
 {
-        public class mymapNode<K, V>
+    public class mymapNode<K, V>
+    {
+        public readonly int size;
+        private readonly LinkedList<keyValues<K, V>>[] items;
+        public mymapNode(int size)
         {
-            public readonly int size;
-            private readonly LinkedList<keyValues<K, V>>[] items;
-            public mymapNode(int size)
-            {
-                this.size = size;
-                this.items = new LinkedList<keyValues<K, V>>[size];
+            this.size = size;
+            this.items = new LinkedList<keyValues<K, V>>[size];
 
-            }
-            public int GetArrayPosition(K key)
+        }
+         public int GetArrayPosition(K key)
+         {
+             int position = key.GetHashCode() % size;
+             return Math.Abs(position);
+         }
+         public V Get(K key)
+         {
+             int position = GetArrayPosition(key);
+             LinkedList<keyValues<K, V>> linkedlist = GetLinkedlist(position);
+             foreach (keyValues<K, V> item in linkedlist)
+             {
+                 if (item.key.Equals(key))
+                 {
+                     return item.Value;
+
+                 }
+             }
+             return default(V);
+
+
+         }
+        public void Add(K key, V value)
+        {
+
+            int position = GetArrayPosition(key);
+            LinkedList<keyValues<K, V>> linkedlist = GetLinkedlist(position);
+            keyValues<K, V> item = new keyValues<K, V>() { key = key, Value = value };
+            linkedlist.AddLast(item);
+        }
+
+        public LinkedList<keyValues<K, V>> GetLinkedlist(int position)
+        {
+            LinkedList<keyValues<K, V>> linkedlist = items[position];
+            if (linkedlist == null)
             {
-                int position = key.GetHashCode() % size;
-                return Math.Abs(position);
+                linkedlist = new LinkedList<keyValues<K, V>>();
+                items[position] = linkedlist;
             }
-            public V Get(K key)
+            return linkedlist;
+        }
+        public void para()
+        {
+
+            String testing = "Paranoids are not paranoid  because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable  situations";
+         
+            Console.Write(testing.IndexOf("because") + Environment.NewLine);
+
+            Console.WriteLine(testing.Substring(testing.IndexOf("because")));
+
+        }
+     
+            public  int  CheckOccurrences(string str1, string pattern)
             {
-                int position = GetArrayPosition(key);
-                LinkedList<keyValues<K, V>> linkedlist = GetLinkedlist(position);
-                foreach (keyValues<K, V> item in linkedlist)
+                int count = 0;
+                int a = 0;
+                while ((a = str1.IndexOf(pattern, a)) != -1)
                 {
-                    if (item.key.Equals(key))
-                    {
-                        return item.Value;
-
-                    }
+                    a += pattern.Length;
+                    count++;
                 }
-                return default(V);
+                return count;
+            }
 
-
-            }
-            public void Add(K key, V value)
-            {
-                int position = GetArrayPosition(key);
-                LinkedList<keyValues<K, V>> linkedlist = GetLinkedlist(position);
-                keyValues<K, V> item = new keyValues<K, V>() { key = key, Value = value };
-                linkedlist.AddLast(item);
-            }
-       
-            public LinkedList<keyValues<K, V>> GetLinkedlist(int position)
-            {
-                LinkedList<keyValues<K, V>> linkedlist = items[position];
-                if (linkedlist == null)
-                {
-                    linkedlist = new LinkedList<keyValues<K, V>>();
-                    items[position] = linkedlist;
-                }
-                return linkedlist;
-            }
-    
-    }
-        public struct keyValues<K, V>
+            public struct keyValues<K, V>
         {
             public K key { get; set; }
             public V Value { get; set; }
         }
     }
+}
 
 
 
